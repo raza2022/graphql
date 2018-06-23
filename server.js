2f87ab3af83853167f9ea56603bb7e095d4561b8
Abdul Hameed <raza2022@gmail.com>
2018-06-23 16:17:58 +0500

    initial query :bowtie:

---
+++ server.js
@@ -0,0 +1,20 @@
+let { graphql, buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+let root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+    console.log(response);
+});
